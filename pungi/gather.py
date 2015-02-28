64b6c8065c36b4d3bf5a38e81a535615ec6fddee
Dennis Gilmore <dennis@ausil.us>
2015-02-28 00:38:18 -0600

    switch to BSD style hashes for the iso checksums

--- pungi/gather.py
+++ pungi/gather.py
@@ -1556,7 +1556,7 @@ class Pungi(PungiBase):
         self.logger.info("Generating checksum of %s" % path)
         checksum = pungi.util._doCheckSum(path, 'sha256', self.logger)
         if checksum:
-            checkfile.write("%s *%s\n" % (checksum.replace('sha256:', ''), os.path.basename(path)))
+            checkfile.write("SHA256 (%s) = %s\n" % (os.path.basename(path), checksum.replace('sha256:', '')))
         else:
             self.logger.error('Failed to generate checksum for %s' % checkfile)
             sys.exit(1)
