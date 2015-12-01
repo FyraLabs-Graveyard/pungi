1ab4144dfb17bd2f36c9dc6ffb174835e5d80749
Dennis Gilmore <dennis@ausil.us>
2015-12-01 11:27:24 -0600

    fix up the paths to the docs
    Signed-off-by: Dennis Gilmore <dennis@ausil.us>

--- doc/update-docs.sh
+++ doc/update-docs.sh
@@ -4,14 +4,14 @@
 # SPDX-License-Identifier:      GPL-2.0
 
 git clone https://pagure.io/pungi.git /tmp/pungi
-pushd /tmp/pungi/docs
+pushd /tmp/pungi/doc
 make html
 popd
 
 git clone ssh://git@pagure.io/docs/pungi.git /tmp/pungi-doc
 pushd /tmp/pungi-doc
 git rm -fr ./*
-cp -r /tmp/pungi/docs/build/html/* ./
+cp -r /tmp/pungi/doc/_build/html/* ./
 git add .
 git commit -s -m "update rendered pungi docs"
 git push origin master
