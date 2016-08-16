a72a38b278e6647a6f9ebb9d90640bfdaf6ab619
Lubomír Sedlář <lsedlar@redhat.com>
2016-08-16 08:02:15 +0200

    Add full Pungi version to log output
    This should help with debugging by providing better information on which
    Pungi version created the compose. In development, the version will show
    output of git describe, in production it asks which version is installed
    in site-packages/. The egg-info directory must be installed for this to
    work.
    
    It is no longer necessary to synchronize version in `setup.py` with
    `pungi/__init__.py`.
    
    Signed-off-by: Lubomír Sedlář <lsedlar@redhat.com>

--- pungi/__init__.py
+++ pungi/__init__.py
@@ -1,1 +1,24 @@
-__version__ = "4.0"
+# -*- coding: utf-8 -*-
+
+import os
+import re
+
+
+def get_full_version():
+    """
+    Find full version of Pungi: if running from git, this will return cleaned
+    output of `git describe`, otherwise it will look for installed version.
+    """
+    location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
+    if os.path.isdir(os.path.join(location, '.git')):
+        import subprocess
+        proc = subprocess.Popen(['git', '-C', location, 'describe', '--tags'],
+                                stdout=subprocess.PIPE)
+        output, _ = proc.communicate()
+        return re.sub(r'-1.fc\d\d?', '', output.strip().replace('pungi-', ''))
+    else:
+        import pkg_resources
+        try:
+            return pkg_resources.get_distribution('pungi').version
+        except pkg_resources.DistributionNotFound:
+            return 'unknown'
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ packages = sorted(packages)
 
 setup(
     name            = "pungi",
-    version         = "4.1.8",  # make sure it matches with pungi.__version__
+    version         = "4.1.8",
     description     = "Distribution compose tool",
     url             = "https://pagure.io/pungi",
     author          = "Dennis Gilmore",
