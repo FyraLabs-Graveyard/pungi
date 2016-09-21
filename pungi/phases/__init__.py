d6dc269ef4cd2a8e68681e8cdb81e9200cd1adf0
Lubomír Sedlář <lsedlar@redhat.com>
2016-09-21 14:49:13 +0200

    Remove FSF address from comments
    The address is no longer correct. We can just as well simply point to
    the web page describing the license.
    
    Signed-off-by: Lubomír Sedlář <lsedlar@redhat.com>

--- pungi/phases/__init__.py
+++ pungi/phases/__init__.py
@@ -11,8 +11,7 @@
 # GNU Library General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# along with this program; if not, see <https://gnu.org/licenses/>.
 
 
 # phases in runtime order
