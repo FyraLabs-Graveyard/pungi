f1b71d1eeb6356bfe81361432b4279ab9f5881a0
Lubomír Sedlář <lsedlar@redhat.com>
2018-05-16 16:25:58 +0200

    arch: Drop mapping ppc64 -> ppc64p7
    The ppc64p7 flavour is not used anymore.
    
    Signed-off-by: Lubomír Sedlář <lsedlar@redhat.com>

--- pungi/arch.py
+++ pungi/arch.py
@@ -18,7 +18,6 @@ from .arch_utils import getBaseArch, getMultiArchInfo, getArchList
 
 TREE_ARCH_YUM_ARCH_MAP = {
     "i386": "i686",
-    "ppc64": "ppc64p7",
     "sparc": "sparc64v",
     "arm": "armv7l",
     "armhfp": "armv7hnl",
